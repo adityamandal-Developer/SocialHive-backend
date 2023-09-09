26aa941461d08b4ab9d6f2c9cabc274311a1f36a
adityamandal-Developer <mandaladitya901@gmail.com>
2023-09-09 12:55:31 +0530

    User model

---
+++ model/User/User.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+
+//schema
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now(),
+    },
+    isVerified: {
+        type: String,
+        default: false,
+    },
+    accountLevel: {
+        type: String,
+        enum: ["bronze", "silver", "gold"],
+        default: bronze,
+    },
+    profilePicture: {
+        type: String,
+        default: "",
+    },
+    coverImage: {
+        type: String,
+        default: "",
+    },
+    bio: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    notificationPreferences: {
+        email: { type: String, default: true },
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female", "prefer not to say", "non-binary"],
+    },
+    profileViewers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+    likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+    passwordResetToken: {
+        type: String,
+    },
+    passwordResetExpires: {
+        type: Date,
+    },
+    accountVerificationToken: {
+        type: String,
+    },
+    accountVerificationExpires: {
+        type: Date,
+    },
+}, {
+    timestamps: true,
+});
+
+//compile schema to model
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
