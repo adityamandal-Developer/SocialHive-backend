ae7137436a3dca04ffc15ff83454a206d46cd0a9
adityamandal-Developer <mandaladitya901@gmail.com>
2023-10-23 17:39:27 +0530

    fixed schema

--- model/User/User.js
+++ model/User/User.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
             default: Date.now(),
         },
         isVerified: {
-            type: Boolean,
+            type: String,
             default: false,
         },
         profilePicture: {
